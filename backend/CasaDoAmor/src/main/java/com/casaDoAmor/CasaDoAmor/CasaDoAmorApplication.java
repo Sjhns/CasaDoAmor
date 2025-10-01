52451786e84f50013dc11dcc50a1325635cc7a15
Johnson <johnsonbarbosa@outlook.com>
2025-09-30 22:11:14 -0300

    init: initialize CasaDoAmor project with Maven structure and basic application files

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/CasaDoAmorApplication.java
@@ -0,0 +1,13 @@
+package com.casaDoAmor.CasaDoAmor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDoAmorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDoAmorApplication.class, args);
+	}
+
+}
---
+++ backend/CasaDoAmor/src/test/java/com/casaDoAmor/CasaDoAmor/CasaDoAmorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.casaDoAmor.CasaDoAmor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CasaDoAmorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
