72037ab51f30f2fcc36e9f4eba43a8b6d2ee9dd3
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-22 19:59:06 -0300

    feat:  criar tabela Estoque com atributos

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Estoque.java
@@ -0,0 +1,20 @@
+package com.casaDoAmor.CasaDoAmor.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import java.util.UUID;
+import lombok.Data;
+
+@Data
+@Entity
+public class Estoque {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID identificador;
+    private long quantidade;
+    private long estoque_minimo;
+    private long estoque_maximo;
+    private String status;
+}
