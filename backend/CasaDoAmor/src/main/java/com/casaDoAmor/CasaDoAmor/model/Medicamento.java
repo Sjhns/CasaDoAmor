bcf1b5439d94b9b0950c378c8ecb08da1a230aeb
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-22 19:59:27 -0300

    feat:  criar tabela Medicamentos com atributos

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Medicamento.java
@@ -0,0 +1,25 @@
+package com.casaDoAmor.CasaDoAmor.model;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import java.util.UUID;
+import java.util.Date;
+
+@Data
+@Entity
+public class Medicamento {
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID identificador;
+    private String nome;
+    private String lote;
+    private String forma_farmaceutica;
+    private String via_de_administracao;
+    private String concentracao;
+    private String categoria_terapeutica;
+    private String laboratorio_fabricante;
+    private Date validade;
+}
