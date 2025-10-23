853c342f3f4ba340b85c41c2af81d971ceefd241
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-23 17:54:31 -0300

    fix: mudar IDENTITY por AUTO, DATE por LOCALDATE

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Medicamento.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Medicamento.java
@@ -1,18 +1,17 @@
 package com.casaDoAmor.CasaDoAmor.model;
-
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
 import java.util.UUID;
-import java.util.Date;
+import java.time.LocalDate;
 
 @Data
 @Entity
 public class Medicamento {
     @Id 
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private String nome;
     private String lote;
@@ -21,5 +20,5 @@ public class Medicamento {
     private String concentracao;
     private String categoria_terapeutica;
     private String laboratorio_fabricante;
-    private Date validade;
+    private LocalDate validade;
 }
