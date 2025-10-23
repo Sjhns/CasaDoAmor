cc2da08256760f73a2d97ace21e2132f11fa9b2a
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-23 10:00:15 -0300

    fix: adicionar campo id e tornar cpf_cnpj unico

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Doador.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Doador.java
@@ -1,6 +1,11 @@
 package com.casaDoAmor.CasaDoAmor.model;
 
+import java.util.UUID;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
 
@@ -8,7 +13,12 @@ import lombok.Data;
 @Entity
 public class Doador {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @Column(unique = true)
     private String CPF_CNPJ;
+    
     private String nome;
     private String telefone;
     private String email;
