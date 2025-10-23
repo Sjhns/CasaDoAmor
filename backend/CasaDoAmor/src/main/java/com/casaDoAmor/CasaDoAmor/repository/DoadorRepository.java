a2867ca782ed6eac0849d4803517d2b5c9d6ee4c
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-23 17:56:27 -0300

    feat: criar repository de Doador

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/DoadorRepository.java
@@ -0,0 +1,16 @@
+package com.casaDoAmor.CasaDoAmor.repository;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.casaDoAmor.CasaDoAmor.model.Doador;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoadorRepository extends JpaRepository<Doador, UUID>{
+    Optional<Doador> findById(UUID id);
+    Doador findByCPF_CNPJ(String CPF_CNPJ);
+    Doador findByNome(String nome);
+    Doador findByTelefone(String telefone);
+    Doador findByEmail(String email);
+
+}
