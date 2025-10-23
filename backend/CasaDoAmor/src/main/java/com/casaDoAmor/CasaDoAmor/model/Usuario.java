8b9609841c2408f6362eddb1f1e22884cfb34298
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-23 09:56:56 -0300

    fix:  adicionar identificador ao usuario

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Usuario.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Usuario.java
@@ -1,11 +1,19 @@
 package com.casaDoAmor.CasaDoAmor.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
 import lombok.Data;
 
 @Data
 @Entity
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
     private String nome;
     private String cargo;
 }
