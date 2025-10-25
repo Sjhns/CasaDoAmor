c0cb4711c60c02f0b2f34e1b83e4c2db1d622d4b
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-25 14:29:59 -0300

    feat: repository do Estoque

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/EstoqueRepository.java
@@ -0,0 +1,17 @@
+package com.casaDoAmor.CasaDoAmor.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.casaDoAmor.CasaDoAmor.model.Estoque;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+
+
+
+@Repository
+public interface EstoqueRepository extends JpaRepository<Estoque, UUID>{
+    Optional <Estoque> findById(UUID id);
+    Estoque findByQuantidade(long quantidade);
+    Estoque findByEstoqueMaximo(long estoqueMaximo);
+    Estoque findByEstoqueMinimo(long estoqueMinimo);
+    Estoque findByStatus(String status);
+}
