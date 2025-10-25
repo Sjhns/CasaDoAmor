3a09088185dadfa61bbdc4ea1415d4e555cfa898
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-25 14:30:31 -0300

    feat: repository do Medicamento

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/MedicamentoRepository.java
@@ -0,0 +1,23 @@
+package com.casaDoAmor.CasaDoAmor.repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.casaDoAmor.CasaDoAmor.model.Medicamento;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.UUID;
+import java.time.LocalDate;
+
+
+
+
+@Repository
+public interface MedicamentoRepository extends JpaRepository<Medicamento, UUID> {
+    Optional<Medicamento> findById(UUID id);
+    Medicamento findByNome(String nome);
+    Medicamento findByLote(String lote);
+    Medicamento findByFormaFarmaceutica(String formaFarmaceutica);
+    Medicamento findByViaDeAdministracao(String viaDeAdministracao);
+    Medicamento findByConcentracao(String concentracao);
+    Medicamento findByCategoriaTerapeutica(String categoriaTerapeutica);
+    Medicamento findByLaboratorioFabricante(String laboratorioFabricante);
+    Medicamento findByValidade(LocalDate validade);
+}
