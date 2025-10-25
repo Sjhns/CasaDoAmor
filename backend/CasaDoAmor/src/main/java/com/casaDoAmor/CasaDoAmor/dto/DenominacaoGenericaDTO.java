6a179dd33fc9bc722916f4a4897522d383d73fa7
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-25 14:32:02 -0300

    feat: dto de Denominacao Generica

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/dto/DenominacaoGenericaDTO.java
@@ -0,0 +1,11 @@
+package com.casaDoAmor.CasaDoAmor.dto;
+import lombok.Getter;
+import lombok.Setter;
+import java.util.UUID;
+
+@Getter
+@Setter
+public class DenominacaoGenericaDTO {
+    private UUID id;
+    private String nome;
+}
