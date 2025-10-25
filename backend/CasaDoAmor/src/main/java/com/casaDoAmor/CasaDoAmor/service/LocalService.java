a9b4ac8022806f3d29c31485789151b0da558bca
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-25 14:31:26 -0300

    feat: service de Local

---
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/service/LocalService.java
@@ -0,0 +1,30 @@
+package com.casaDoAmor.CasaDoAmor.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.casaDoAmor.CasaDoAmor.model.Local;
+import com.casaDoAmor.CasaDoAmor.repository.LocalRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class LocalService {
+    private final LocalRepository localRepository;
+    public LocalService(LocalRepository localRepository) {
+        this.localRepository = localRepository;
+    }
+    @Transactional
+    public Local salvar(Local local) {
+        return localRepository.save(local);
+    }
+    @Transactional
+    public void deletar(Local local) {
+        localRepository.delete(local);
+    }
+    @Transactional
+    public List<Local> listarTodos() {
+        return localRepository.findAll();
+    }
+}
