6ff55388f8c8b04d76bf3d7fd1bfae883d379378
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-29 19:28:29 -0300

    feat: adicionar propriedade não nulo

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/dto/EstoqueDTO.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/dto/EstoqueDTO.java
@@ -2,13 +2,21 @@ package com.casaDoAmor.CasaDoAmor.dto;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.UUID;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
 public class EstoqueDTO {
     private UUID id;
-    private long quantidade;
-    private long estoqueMinimo;
-    private long estoqueMaximo;
+    @NotNull
+    @Min(0)
+    private Long quantidade;
+    @Min(0)
+    private Long estoqueMinimo;
+    @Min(0)
+    private Long estoqueMaximo;
+    @NotBlank
     private String status;
 }
