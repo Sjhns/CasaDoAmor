a8918cf288fa75240719ba6cab2d856717d56809
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-10-29 19:28:25 -0300

    feat: adicionar propriedade não nulo

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/dto/DoadorDTO.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/dto/DoadorDTO.java
@@ -2,13 +2,20 @@ package com.casaDoAmor.CasaDoAmor.dto;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.UUID;
+import jakarta.validation.constraints.NotBlank;
 
 @Getter
 @Setter
 public class DoadorDTO {
     private UUID id;
+    @NotBlank
     private String cpfCnpj;
+    @NotBlank
     private String nome;
+    @NotBlank
     private String email;
+    @NotBlank
     private String telefone;
+    @NotBlank
+    private String endereco;
 }
