fce788a82b3a5da495a79e1f45de87890a5e5ee4
Johnson <108435026+Sjhns@users.noreply.github.com>
2025-11-03 18:48:23 -0300

    refactor: update DenominacaoGenericaController to handle ID in DTO correctly

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/controller/DenominacaoGenericaController.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/controller/DenominacaoGenericaController.java
@@ -1,5 +1,6 @@
 package com.casaDoAmor.CasaDoAmor.controller;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,21 +22,22 @@ public class DenominacaoGenericaController {
     }
     private DenominacaoGenerica paraModel(DenominacaoGenericaDTO dto) {
         DenominacaoGenerica entidade = new DenominacaoGenerica();
-        entidade.setId(dto.getId()); 
+
         entidade.setNome(dto.getNome());
         return entidade;
     }
     private DenominacaoGenericaDTO paraDTO(DenominacaoGenerica entidade) {
         DenominacaoGenericaDTO dto = new DenominacaoGenericaDTO();
-        dto.setId(entidade.getId());
         dto.setNome(entidade.getNome());
+        dto.setId(entidade.getId());
+
         return dto;
     }
     @PostMapping
-    public ResponseEntity<DenominacaoGenericaDTO> salvar(@RequestBody DenominacaoGenericaDTO dto) {
+    public ResponseEntity<UUID> salvar(@RequestBody DenominacaoGenericaDTO dto) {
         DenominacaoGenerica entidadeParaSerSalva = paraModel(dto); 
         DenominacaoGenerica entidadeSalva = denominacaoGenericaService.salvar(entidadeParaSerSalva);
-        return ResponseEntity.status(HttpStatus.CREATED).body(paraDTO(entidadeSalva));
+        return ResponseEntity.status(HttpStatus.CREATED).body(entidadeSalva.getId());
     }
     @GetMapping
     public ResponseEntity<List<DenominacaoGenericaDTO>> listar() {
