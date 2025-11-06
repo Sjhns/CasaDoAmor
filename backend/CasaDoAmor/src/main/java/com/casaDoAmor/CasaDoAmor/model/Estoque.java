f9b4430fa7c10d7c8765900d01c27f3559a64ddf
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-06 20:28:54 -0300

    feat: variaveis corrijidas e identificador sendo puxado da FK medicamento

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Estoque.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Estoque.java
@@ -1,19 +1,29 @@
 package com.casaDoAmor.CasaDoAmor.model;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+
+import java.time.LocalDate;
 import java.util.UUID;
+
 import lombok.Data;
 
 @Data
 @Entity
 public class Estoque {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     private Long quantidade;
     private Long estoqueMinimo;
     private Long estoqueMaximo;
     private String status;
+    private String lote;
+    private LocalDate validadeAposAberto;
+
+    @OneToOne
+    @MapsId
+    @JoinColumn(name = "medicamento_id", nullable = false)
+    private Medicamento medicamento;
 }
