635668a3d101c87799c15ec0a78cf7082bbfebd2
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-06 20:53:26 -0300

    feat: retirar find das variaveis que não existem

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/MedicamentoRepository.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/MedicamentoRepository.java
@@ -4,7 +4,7 @@ import com.casaDoAmor.CasaDoAmor.model.Medicamento;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 import java.util.UUID;
-import java.time.LocalDate;
+
 
 
 
@@ -12,11 +12,9 @@ import java.time.LocalDate;
 public interface MedicamentoRepository extends JpaRepository<Medicamento, UUID> {
     Optional<Medicamento> findById(UUID id);
     Medicamento findByNome(String nome);
-    Medicamento findByLote(String lote);
     Medicamento findByFormaFarmaceutica(String formaFarmaceutica);
     Medicamento findByViaDeAdministracao(String viaDeAdministracao);
     Medicamento findByConcentracao(String concentracao);
     Medicamento findByCategoriaTerapeutica(String categoriaTerapeutica);
     Medicamento findByLaboratorioFabricante(String laboratorioFabricante);
-    Medicamento findByValidade(LocalDate validade);
 }
