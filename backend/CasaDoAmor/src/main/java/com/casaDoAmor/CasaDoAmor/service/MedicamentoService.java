f6b2459182616d122dfa18d3e0bf8fc0a5f2a2ca
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-06 21:01:48 -0300

    fix: trocar import do transaction para do spring

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/service/MedicamentoService.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/service/MedicamentoService.java
@@ -1,19 +1,23 @@
 package com.casaDoAmor.CasaDoAmor.service;
 
 import java.util.List;
-
+import java.util.UUID;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.casaDoAmor.CasaDoAmor.dtoRequest.MovimentarEstoqueDTORequest;
+import com.casaDoAmor.CasaDoAmor.model.Estoque;
 import com.casaDoAmor.CasaDoAmor.model.Medicamento;
+import com.casaDoAmor.CasaDoAmor.repository.EstoqueRepository;
 import com.casaDoAmor.CasaDoAmor.repository.MedicamentoRepository;
-
-import jakarta.transaction.Transactional;
 
 @Service
 public class MedicamentoService {
-    private final MedicamentoRepository medicamentoRepository;
-    public MedicamentoService(MedicamentoRepository medicamentoRepository) {
+    private final MedicamentoRepository medicamentoRepository;    
+    private final EstoqueRepository estoqueRepository;
+    public MedicamentoService(MedicamentoRepository medicamentoRepository, EstoqueRepository estoqueRepository) {
         this.medicamentoRepository = medicamentoRepository;
+        this.estoqueRepository = estoqueRepository;
     }
     @Transactional
     public Medicamento salvar(Medicamento medicamento) {
@@ -26,5 +30,38 @@ public class MedicamentoService {
     @Transactional
     public List<Medicamento> listarTodos() {
         return medicamentoRepository.findAll();
+    }
+    @Transactional
+    public Estoque movimentarEstoque(UUID medicamentoId, MovimentarEstoqueDTORequest dto){
+        Estoque estoque = estoqueRepository.findByMedicamentoId(medicamentoId)
+            .orElseThrow(() -> new RuntimeException("ERRO 404: Estoque não encontrado para o ID: " + medicamentoId)); 
+        long quantidadeMovimentada = dto.getQuantidade();
+        long quantidadeAtual = estoque.getQuantidade();
+        long quantidadeMinima = estoque.getEstoqueMinimo();
+        long quantidadeMaxima = estoque.getEstoqueMaximo();
+        long novoSaldo;
+        if (quantidadeMovimentada <= 0) {
+            throw new RuntimeException("ERRO 400: A quantidade a ser movimentada deve ser maior que zero.");
+        }
+        switch (dto.getTipo().toUpperCase()) {
+            case "ENTRADA":
+                novoSaldo = quantidadeAtual + quantidadeMovimentada;
+                if (novoSaldo > quantidadeMaxima) {
+                    throw new RuntimeException("ENTRADA INVÁLIDA: O saldo de " + novoSaldo + 
+                                            " ultrapassa o estoque máximo (" + quantidadeMaxima + ")."); 
+                }
+                break;
+            case "SAIDA":
+                novoSaldo = quantidadeAtual - quantidadeMovimentada;
+                if (novoSaldo < quantidadeMinima) {
+                    throw new RuntimeException("SAÍDA INVÁLIDA: O saldo de " + novoSaldo + 
+                                            " é inferior ao estoque mínimo (" + quantidadeMinima + ").");
+                }
+                break;
+            default:
+                throw new RuntimeException("ERRO 400: Tipo de movimentação inválido: " + dto.getTipo() + ".");
+        }
+        estoque.setQuantidade(novoSaldo);
+        return estoqueRepository.save(estoque);
     }
 }
