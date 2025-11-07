7cf862dcb0039fffd74d151e71ba9588d19a3bf3
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-06 21:01:40 -0300

    fix: trocar import do transaction para do spring

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/service/EstoqueService.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/service/EstoqueService.java
@@ -1,24 +1,32 @@
 package com.casaDoAmor.CasaDoAmor.service;
-
 import java.util.List;
-
+import java.util.UUID;
 import org.springframework.stereotype.Service;
-
 import com.casaDoAmor.CasaDoAmor.model.Estoque;
+import com.casaDoAmor.CasaDoAmor.model.Medicamento;
 import com.casaDoAmor.CasaDoAmor.repository.EstoqueRepository;
-
-import jakarta.transaction.Transactional;
+import com.casaDoAmor.CasaDoAmor.repository.MedicamentoRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class EstoqueService {
-    private final EstoqueRepository estoqueRepository;
-    public EstoqueService(EstoqueRepository estoqueRepository) {
+private final EstoqueRepository estoqueRepository;
+    private final MedicamentoRepository medicamentoRepository;
+    public EstoqueService(EstoqueRepository estoqueRepository, MedicamentoRepository medicamentoRepository) {
         this.estoqueRepository = estoqueRepository;
+        this.medicamentoRepository = medicamentoRepository;
     }
     @Transactional
     public Estoque salvar(Estoque estoque) {
-        return estoqueRepository.save(estoque);
+        UUID medicamentoId = estoque.getId();
+        Medicamento medicamento = medicamentoRepository.findById(medicamentoId)
+             .orElseThrow(() -> new RuntimeException("Medicamento não encontrado com ID: " + medicamentoId)); 
+        estoque.setMedicamento(medicamento);
+        medicamento.setEstoque(estoque);
+        Medicamento medicamentoSalvo = medicamentoRepository.save(medicamento);
+        return medicamentoSalvo.getEstoque();
     }
+    
     @Transactional
     public void deletar(Estoque estoque) {
         estoqueRepository.delete(estoque);
