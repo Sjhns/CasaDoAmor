76abd6a99b331938fb841c0f190dde641e8bdb14
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-06 21:10:07 -0300

    fix: retirar paraDTO e usar fromEntity

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/controller/LocalController.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/controller/LocalController.java
@@ -1,7 +1,6 @@
 package com.casaDoAmor.CasaDoAmor.controller;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
@@ -12,7 +11,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.casaDoAmor.CasaDoAmor.dto.LocalDTO;
+import com.casaDoAmor.CasaDoAmor.dtoRequest.LocalDTORequest;
+import com.casaDoAmor.CasaDoAmor.dtoResponse.LocalDTOResponse;
 import com.casaDoAmor.CasaDoAmor.model.Local;
 import com.casaDoAmor.CasaDoAmor.service.LocalService;
 
@@ -23,28 +23,24 @@ public class LocalController {
     public LocalController(LocalService localService) {
         this.localService = localService;
     }
-    private Local paraModel(LocalDTO dto) {
+    private Local paraModel(LocalDTORequest dto) {
         Local entidade = new Local();
+        entidade.setId(dto.getId());
         entidade.setNome(dto.getNome());
         return entidade;
     }
-    private LocalDTO paraDTO(Local entidade) {
-        LocalDTO dto = new LocalDTO();
-        dto.setNome(entidade.getNome());
-        dto.setId(entidade.getId());
-        return dto;
-    }
     @PostMapping
-    public ResponseEntity<UUID> salvar(@RequestBody LocalDTO dto) {
+    public ResponseEntity<LocalDTOResponse> salvar(@RequestBody LocalDTORequest dto) {
         Local entidadeParaSerSalva = paraModel(dto); 
         Local entidadeSalva = localService.salvar(entidadeParaSerSalva);
-        return ResponseEntity.status(HttpStatus.CREATED).body(entidadeSalva.getId());
+        LocalDTOResponse responseDTO = LocalDTOResponse.fromEntity(entidadeSalva); 
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
     @GetMapping
-    public ResponseEntity<List<LocalDTO>> listar() {
+    public ResponseEntity<List<LocalDTOResponse>> listar() {
         List<Local> entidades = localService.listarTodos();
-        List<LocalDTO> locais = entidades.stream()
-            .map(this::paraDTO)
+        List<LocalDTOResponse> locais = entidades.stream()
+            .map(LocalDTOResponse::fromEntity)
             .collect(Collectors.toList());
         return ResponseEntity.ok(locais);
     }
