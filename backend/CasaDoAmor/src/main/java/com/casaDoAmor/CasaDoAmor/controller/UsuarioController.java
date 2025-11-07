46af20e46aa2d516b98abecede712fcd372c45aa
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-06 21:10:03 -0300

    fix: retirar paraDTO e usar fromEntity

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/controller/UsuarioController.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/controller/UsuarioController.java
@@ -1,7 +1,6 @@
 package com.casaDoAmor.CasaDoAmor.controller;
 
 import java.util.List;
-import java.util.UUID;
 import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
@@ -12,49 +11,39 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.casaDoAmor.CasaDoAmor.dto.UsuarioDTO;
+import com.casaDoAmor.CasaDoAmor.dtoRequest.UsuarioDTORequest;
+import com.casaDoAmor.CasaDoAmor.dtoResponse.UsuarioDTOResponse;
 import com.casaDoAmor.CasaDoAmor.model.Usuario;
 import com.casaDoAmor.CasaDoAmor.service.UsuarioService;
-
-
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
     UsuarioService usuarioService;
-
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
-
-    private Usuario paraModel(UsuarioDTO dto) {
+    private Usuario paraModel(UsuarioDTORequest dto) {
         Usuario entidade = new Usuario();
+        entidade.setId(dto.getId());
         entidade.setNome(dto.getNome());
         entidade.setCargo(dto.getCargo());
         return entidade;
     }
-
-    private UsuarioDTO paraDTO(Usuario entidade) {
-        UsuarioDTO dto = new UsuarioDTO();
-        dto.setNome(entidade.getNome());
-        dto.setCargo(entidade.getCargo());
-        dto.setId(entidade.getId());
-        return dto;
-    }
-
     @PostMapping
-    public ResponseEntity<UUID> salvar(@RequestBody UsuarioDTO dto) {
-        Usuario entidadeParaSerSalva = paraModel(dto);
+    public ResponseEntity<UsuarioDTOResponse> salvar(@RequestBody UsuarioDTORequest dto) {
+        Usuario entidadeParaSerSalva = paraModel(dto); 
         Usuario entidadeSalva = usuarioService.salvar(entidadeParaSerSalva);
-        return ResponseEntity.status(HttpStatus.CREATED).body(entidadeSalva.getId());
+        UsuarioDTOResponse responseDTO = UsuarioDTOResponse.fromEntity(entidadeSalva);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
     }
-
+    
     @GetMapping
-    public ResponseEntity<List<UsuarioDTO>> listar() {
+    public ResponseEntity<List<UsuarioDTOResponse>> listar() {
         List<Usuario> entidades = usuarioService.listarTodos();
-        List<UsuarioDTO> usuarios = entidades.stream()
-                .map(this::paraDTO)
-                .collect(Collectors.toList());
+        List<UsuarioDTOResponse> usuarios = entidades.stream()
+            .map(UsuarioDTOResponse::fromEntity)
+            .collect(Collectors.toList());
         return ResponseEntity.ok(usuarios);
     }
 }
