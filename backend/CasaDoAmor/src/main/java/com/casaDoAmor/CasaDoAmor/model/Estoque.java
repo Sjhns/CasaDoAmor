34c719c64ed95a54c30a4adfb6360b42dbbc95ff
Rafael Pinto Neves Ferreira <rafaelpfn@hotmail.com>
2025-11-13 11:24:31 -0300

    feat: colunas retiradas, id gerado automaticamente, cardinalidade many to one

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Estoque.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/model/Estoque.java
@@ -1,29 +1,26 @@
 package com.casaDoAmor.CasaDoAmor.model;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
-
+import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
 import java.util.UUID;
-
 import lombok.Data;
 
 @Data
 @Entity
 public class Estoque {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private Long quantidade;
-    private Long estoqueMinimo;
-    private Long estoqueMaximo;
     private String status;
     private String lote;
     private LocalDate validadeAposAberto;
 
-    @OneToOne
-    @MapsId
+    @ManyToOne
     @JoinColumn(name = "medicamento_id", nullable = false)
     private Medicamento medicamento;
 }
