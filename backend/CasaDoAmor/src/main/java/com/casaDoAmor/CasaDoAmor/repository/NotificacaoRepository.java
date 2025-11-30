e86fcf68a292684810f7e1086b4bbaaabe94ce47
PATRIQUE-source <patrique.rodrigues.n@gmail.com>
2025-11-30 15:48:16 -0300

    fix: add missing import for Notificacao model in NotificacaoRepository

--- backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/NotificacaoRepository.java
+++ backend/CasaDoAmor/src/main/java/com/casaDoAmor/CasaDoAmor/repository/NotificacaoRepository.java
@@ -1,7 +1,10 @@
 package com.casaDoAmor.CasaDoAmor.repository;
 
-import com.casaDoAmor.CasaDoAmor.model.Notificacao; // <-- ESTE É O IMPORT QUE FALTAVA
+import com.casaDoAmor.CasaDoAmor.model.Notificacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface NotificacaoRepository extends JpaRepository<Notificacao, Long> {
+    List<Notificacao> findByLidaFalseOrderByDataCriacaoDesc();
+    List<Notificacao> findAllByOrderByDataCriacaoDesc();
 }
